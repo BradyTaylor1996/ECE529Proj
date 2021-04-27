9c7206629ee84ff1136b55ce0eeed3af4b3f8849
EricLeeZR <ziru.li@duke.edu>
2021-04-26 23:43:08 -0400

    Add files via upload

---
+++ make_array.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+	int sum;
+	int i;
+	int j;
+	ofstream ofile;
+	ofile.open("make_array.txt");
+	for (i = 255; i >= 0; i--)
+		for (j = 255; j >= 0; j--)
+	{
+		ofile << 'X' << i <<'_'<<j<< ' '<<'b'<<i<<' ' << "b_bar" << i << ' ' << "wl" << j << ' '<<"vdd gnd sram_cell"<<'\n';
+	}
+	ofile << '\n';
+
+	/*
+	for (sum = 255; sum >= 0; sum--)
+	{
+		ofile << "b" << sum << ' ';
+	}
+	ofile << '\n';
+
+	for (sum = 255; sum >= 0; sum--)
+	{
+		ofile << "b_bar" << sum << ' ';
+	}
+	ofile << '\n';
+
+	for (sum = 255; sum >= 0; sum--)
+	{
+		ofile << "wl" << sum << ' ';
+	}
+	ofile << '\n';
+	*/
+	ofile.close();
+	return 0;
+}
